23e9c6e3e0ba07a91f36129152d9f6716f7b6316
jairo <jairo.castaneda@uptc.edu.co>
2020-12-11 01:26:43 -0500

    ejercicio juegos

---
+++ functions.js
@@ -0,0 +1,79 @@
+$(document).ready(function (){
+    var aux=0,aux2= [0,0,0];
+    var cadena="";
+    $("#hola").click(function (){
+        $("#hola").css("color","red");
+        $("#hola").css("background-color","yellow");
+        }
+    );
+    $("#foto").click(function (){
+        $("#foto").hide();
+    });
+
+    $("#halo").click(function(){
+        aux++;cadena="";aux2[0]++;
+        
+        if(aux2[0]<4){
+            if(aux2[0!=3]){
+                if(aux%2==0){
+                    
+                    $(".halo").css("border","10px");
+                    $(".halo").css("border-style","solid");
+                    $(".halo").css("border-color","pink");
+                    
+                    cadena+="<tr><td>"+aux+"</td><td>"+"Halo2"+"</td>"+"<td>"+"Rosa"+"</td><td>"+aux2[0]+"</td></tr>"
+                }
+                else{
+                        
+                    $(".halo").css("border","10px");
+                    $(".halo").css("border-style","solid");
+                    $(".halo").css("border-color","red");
+                    cadena+="<tr><td>"+aux+"</td><td>"+"Halo2"+"</td>"+"<td>"+"Rojo"+"</td><td>"+aux2[0]+"</td></tr>"
+                }
+                $("#tabla").append(cadena);      
+            }
+            else{
+                $(".halo").css("border","10px");
+                $(".halo").css("border-style","solid");
+                $(".halo").css("border-color","red");
+                cadena+="<tr class="+'table-warning'+"><td>"+aux+"</td><td>"+"Halo2"+"</td>"+"<td>"+"Rojo"+"</td><td>"+aux2[0]+"</td></tr>"
+                $("#tabla").append(cadena);
+            }
+        }
+       
+
+        else{
+            $("#halo").hide();
+            $(".halo").hide();
+        }
+        
+    });
+    $("#gears").click(function(){
+        aux++;cadena="";aux2[1]++;
+        $(".gears").css("border","10px");
+        $(".gears").css("border-style","solid");
+        $(".gears").css("border-color","blue");
+
+        cadena+="<tr><td>"+aux+"</td><td>"+"Gears"+"</td>"+"<td>"+"Azul"+"</td><td>"+aux2[1]+"</td></tr>"
+        console.log(cadena);
+    $("#tabla").append(cadena);
+    });
+    $("#fifa").click(function(){
+        aux++;cadena="";aux2[2]++;
+        $(".fifa").css("border","10px");
+        $(".fifa").css("border-style","solid");
+        $(".fifa").css("border-color","green");
+
+        cadena+="<tr><td>"+aux+"</td><td>"+"Fifa"+"</td>"+"<td>"+"Verde"+"</td><td>"+aux2[2]+"</td></tr>"
+    console.log(cadena);
+    $("#tabla").append(cadena);
+    cadena="";
+    });
+
+    /*for(var x=0;x<=aux;x++){
+        cadena+="<tr><td>"+x+"</td>"+"<td>"+x+1+"</td></tr>"
+    }*/
+    //cadena+="</table>";
+    
+
+});
